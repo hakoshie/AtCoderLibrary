77d1c6de4c56aa1379c2c53b843a693b0600a4d5
hakoshie <hakoshie@gmail.com>
2025-03-05 21:19:15 +0900

    edited comment of dijkstra

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -16,36 +16,36 @@ vector<edge> G[MAX_V];
 int d[MAX_V];
 
 void dijkstra(int s) {
-	//sからiへの最短距離をd[i]に保存 i=v_i
+	//sからiへの最短距離をd[i]に保存 d[i] means the path from s to i costs d[i]
 	priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > que;
 	fill(d, d + V, INF);
 	d[s] = 0;//初期位置へのコストは0
 
 	que.push(pair<int,int>(0, s));
-	//queはmake_pair(cost,to)?
+	//queはmake_pair(cost,destination)
 
 	while (!que.empty()) {
-		pair<int,int> p = que.top();
+		auto [cost_v,v] = que.top();
 		que.pop();
-		int v = p.second;//v=p.second from s to v
-		//この候補が追加されてから，見るまでに，sからp.secondへのよりいい経路が見つかっているなら無視
-		if (d[v] < p.first) continue;
-		//v=p.second 
+		//from s to v costs cost_v
+		//この候補が追加されてから，見るまでに，sからvへのよりいい経路が見つかっているなら無視
+		if (d[v] < cost_v) continue;
+
 		for (int i = 0; i < (int)G[v].size(); ++i) {
 			edge e = G[v][i];
-			//e : i番目の v と v_i を結ぶ辺　
-			// G[v][i]=(v_i, v_to_vi_cost)
-			// e.to=v_i
-			int v_i=e.to;
-			int v_to_vi_cost=e.cost;
-	        	// v_to_vi_cost means v to v_i costs v_to_vi_cost
-			if (d[v_i] > d[v] + v_to_vi_cost) {
-				d[v_i] = d[v] + v_to_vi_cost;
-				//from s to v_i > from s to v + from v to v_i
+			//e : v と w を結ぶ辺　
+			// G[v][i]=(w, v_to_w_cost)
+			// e.to=w
+			int w=e.to;
+			int v2w_cost=e.cost;
+	        	// v2w_cost means v to w costs v2w_cost
+			if (d[w] > d[v] + v2w_cost) {
+				d[w] = d[v] + v2w_cost;
+				//from s to w > from s to v + from v to w
 				//vを経由したほうがコストが低いなら経由する
-				que.push(pair<int,int>(d[v_i], v_i));
-				// d[v_i]が更新されたならば，
-				// v_i とつながっているところも見る
+				que.push({d[w], w});
+				// d[w]が更新されたならば，
+				// w とつながっているところも見る
 			}
 		}
 	}
